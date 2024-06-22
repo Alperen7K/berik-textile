9c79bb550e89e5339575d24a47965044e1cb7aa8
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-06-22 10:31:42 +0300

    Navbar is done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/berik-textile.png";
+import { LanguageSwitch } from "./LanguageSwitch";
+
+export const Navbar = () => {
+ return (
+  <div className="w-full py-4 flex justify-center bg-white items-center fixed top-0 z-40 shadow-xl">
+   <div className="w-[90vw] sm:w-[95vw] flex justify-between items-center">
+    <img src={logo} className=" w-48 sm:w-64 object-contain" />
+    <LanguageSwitch />
+   </div>
+  </div>
+ );
+};
